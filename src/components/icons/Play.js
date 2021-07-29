4b211c53800d17bfa85ec2813eed6a5ee9a6f51f
Jorge Kunrath <jorgeks95@gmail.com>
2021-07-29 04:20:42 -0300

    add icons and assets

---
+++ src/components/icons/Play.js
@@ -0,0 +1,12 @@
+export default function IconPlay() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="none" viewBox="0 0 16 20">
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M15.41 9.05L1.585.3C1.327.15 1.217 0 .922 0 .516 0 0 .35 0 1v18c0 .65.516 1 .922 1 .295 0 .405-.15.663-.3l13.825-8.75c.295-.2.59-.4.59-.95 0-.55-.295-.75-.59-.95z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
---
+++ src/images.js
@@ -0,0 +1,25 @@
+import imageSection1 from "./assets/image-section1.png";
+import imageSection1Big from "./assets/image-section1-big.png";
+import imageSection3 from "./assets/image-section3.png";
+import imageSection3Big from "./assets/image-section3-big.png";
+import imageSection4 from "./assets/image-section4.png";
+import imageSection5 from "./assets/image-section5.png";
+import imageFeaturedHero from "./assets/image-featured-hero.png";
+import imageHeroDesk from "./assets/hero-bk-desktop.png";
+import imageHeroMob from "./assets/hero-bk-mobile.png";
+import profile from "./assets/profile-image.png";
+
+const images = {
+  macMockup: imageSection1,
+  macMockupBig: imageSection1Big,
+  form: imageSection3,
+  formBig: imageSection3Big,
+  table: imageSection4,
+  diary: imageSection5,
+  featured: imageFeaturedHero,
+  heroDesk: imageHeroDesk,
+  heroMob: imageHeroMob,
+  profile,
+};
+
+export default images;
